e510e3aea76fd3c2a71c2d908b27c33257aa49f2
Bartjan Henkemans <b.henkemans@student.tue.nl>
2020-12-31 15:23:34 +0100

    Script writes to CSV file
    The python file now writes to a CSV file instead of printing to the console. This way we may produce graphs to analyze the individual experiments instead of just arriving on a probability result.

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from Person import Person  # Custom person class
-import numpy as np  # Numpy
 import time
+import csv
 
 start = time.time()  # Start timer
 
-amountOfPeople = 10000  # Amount of random "second" people that will be generated
+amountOfPeople = 100  # Amount of random "second" people that will be generated
 amountOfExperiments = 100000  # Amount of random people that will be generated and compared
-probArray = []  # Empty array which will store the results of the experiments
 
-for i in range(amountOfExperiments):
-    newPerson = Person()  # New person is generated
-    match = 0  # Variable which will store the amount of matches
-    for j in range(amountOfPeople):
-        if newPerson.isPersonEqual(secondperson=Person(), err=0.1):  # If statement which checks if people match
-            match += 1  # If a match occurs, increase the match counter
-    probArray.append(match / amountOfPeople)  # At the end of an experiment, add the information to the array
+with open('result.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',', quotechar='|',quoting=csv.QUOTE_MINIMAL)  # CSV writer
+    filewriter.writerow(['experiment', 'amountpeople', 'match'])  # Initialize CSV-file
 
-averageProb = np.mean(probArray)  # Generate the average probability
-print(averageProb)  # Print the average probability
+    for i in range(amountOfExperiments):
+        newPerson = Person()  # New person is generated
+        match = 0  # Variable which will store the amount of matches
+        for j in range(amountOfPeople):
+            if newPerson.isPersonEqual(secondperson=Person(), err=0.1):  # If statement which checks if people match
+                match += 1  # If a match occurs, increase the match counter
+        filewriter.writerow([i, amountOfPeople, match])  # Write experiment result to CSV file
 
 end = time.time()  # End timer
-print("{time: .2f}s elapsed".format(time = end - start))  # Print time elapsed
+print("{time: .2f}s elapsed".format(time=end - start))  # Print time elapsed
