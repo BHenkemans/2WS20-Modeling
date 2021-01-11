bf48cea7fe617460ed235bbba27e4b60f9bca1ec
Bartjan Henkemans <b.henkemans@student.tue.nl>
2021-01-11 15:48:27 +0100

    relError can now be changed from main, Also it is now checked if either newPerson or secondperson has an iPhone and would be vulnerable for a "doppelganger"

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ amountOfExperiments = 100000  # Amount of random people that will be generated a
 
 with open('result.csv', 'w', newline='', encoding='utf-8') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',', quotechar='|',quoting=csv.QUOTE_MINIMAL)  # CSV writer
-    filewriter.writerow(['experiment', 'amountpeople', 'match'])  # Initialize CSV-file
+    filewriter.writerow(['experiment', 'amountpeople', 'match', 'iPhone'])  # Initialize CSV-file
 
     for i in range(amountOfExperiments):
         newPerson = Person()  # New person is generated
         match = 0  # Variable which will store the amount of matches
+        matchAndIphone = 0
         for j in range(amountOfPeople):
-            if newPerson.isPersonEqual(secondperson=Person(), err=0.1):  # If statement which checks if people match
+            secondperson = Person()
+            if newPerson.isPersonEqualRelErr(secondperson=secondperson, relError=0):  # If statement which checks if people match
                 match += 1  # If a match occurs, increase the match counter
-        filewriter.writerow([i, amountOfPeople, match])  # Write experiment result to CSV file
+                if newPerson.isVulnerable() or secondperson.isVulnerable():
+                    matchAndIphone += 1
+        filewriter.writerow([i, amountOfPeople, match, matchAndIphone])  # Write experiment result to CSV file
 
 end = time.time()  # End timer
 print("{time: .2f}s elapsed".format(time=end - start))  # Print time elapsed
